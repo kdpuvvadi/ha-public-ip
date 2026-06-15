02cd860c9b180131ba008f51b5ddf4ad9521cfad
KD Puvvadi <7326255+kdpuvvadi@users.noreply.github.com>
2026-06-15 12:51:20 +0530

    feat: manual selection of provider

--- custom_components/public_ip/config_flow.py
+++ custom_components/public_ip/config_flow.py
@@ -33,7 +33,46 @@ class PublicIPConfigFlow(
                 {
                     vol.Required(
                         CONF_PROVIDER,
-                        default="ipify",
+                        default="auto",
+                    ): vol.In(PROVIDERS)
+                }
+            ),
+        )
+
+    @staticmethod
+    def async_get_options_flow(config_entry):
+        return PublicIPOptionsFlow(config_entry)
+
+
+class PublicIPOptionsFlow(
+    config_entries.OptionsFlow
+):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(
+        self,
+        user_input=None,
+    ):
+        if user_input is not None:
+            return self.async_create_entry(
+                title="",
+                data=user_input,
+            )
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        CONF_PROVIDER,
+                        default=self.config_entry.options.get(
+                            CONF_PROVIDER,
+                            self.config_entry.data.get(
+                                CONF_PROVIDER,
+                                "auto",
+                            ),
+                        ),
                     ): vol.In(PROVIDERS)
                 }
             ),
