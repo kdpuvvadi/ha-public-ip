91277f02049b92917020e8a4815988011768822a
KD Puvvadi <7326255+kdpuvvadi@users.noreply.github.com>
2026-06-15 11:31:07 +0530

    refactor: PublicIPSensor

--- custom_components/public_ip/sensor.py
+++ custom_components/public_ip/sensor.py
@@ -1,15 +1,29 @@
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from urllib.parse import urlparse
+
+from homeassistant.components.sensor import (
+    SensorEntity,
+)
+from homeassistant.helpers.device_registry import (
+    DeviceInfo,
+)
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+)
 
 from .const import DOMAIN
 
 
-async def async_setup_entry(hass, entry, async_add_entities):
-    coordinator = hass.data[DOMAIN][entry.entry_id]
+async def async_setup_entry(
+    hass,
+    entry,
+    async_add_entities,
+):
+    coordinator = hass.data[DOMAIN][
+        entry.entry_id
+    ]
 
     async_add_entities(
-        [PublicIPSensor(coordinator)],
-        True,
+        [PublicIPSensor(coordinator)]
     )
 
 
@@ -17,12 +31,10 @@ class PublicIPSensor(
     CoordinatorEntity,
     SensorEntity,
 ):
-    """Representation of the public IP sensor."""
-
-    _attr_name = "Public IP"
-    _attr_unique_id = "public_ip"
-    _attr_icon = "mdi:ip-network"
     _attr_has_entity_name = True
+    _attr_name = "IP Address"
+    _attr_unique_id = "public_ip_address"
+    _attr_icon = "mdi:ip-network"
 
     @property
     def native_value(self):
@@ -31,5 +43,27 @@ class PublicIPSensor(
     @property
     def extra_state_attributes(self):
         return {
-            "provider": self.coordinator.data["provider"],
+            "provider": self.coordinator.data[
+                "provider"
+            ]
         }
+
+    @property
+    def device_info(self):
+        url = self.coordinator.data["url"]
+
+        parsed = urlparse(url)
+
+        configuration_url = (
+            f"{parsed.scheme}://{parsed.netloc}"
+        )
+
+        return DeviceInfo(
+            identifiers={
+                (DOMAIN, "public_ip")
+            },
+            name="Public IP",
+            manufacturer="KD Puvvadi",
+            model="Internet Connection",
+            configuration_url=configuration_url,
+        )
