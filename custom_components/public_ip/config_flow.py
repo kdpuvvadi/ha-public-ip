321e861da7ecfac7e9711fb17f49e75ed8185532
KD Puvvadi <7326255+kdpuvvadi@users.noreply.github.com>
2026-06-15 11:29:57 +0530

    refactor: async_step_user

--- custom_components/public_ip/config_flow.py
+++ custom_components/public_ip/config_flow.py
@@ -1,6 +1,12 @@
+import voluptuous as vol
+
 from homeassistant import config_entries
 
-from .const import DOMAIN
+from .const import (
+    CONF_PROVIDER,
+    DOMAIN,
+    PROVIDERS,
+)
 
 
 class PublicIPConfigFlow(
@@ -9,16 +15,26 @@ class PublicIPConfigFlow(
 ):
     VERSION = 1
 
-    async def async_step_user(
-        self,
-        user_input=None,
-    ):
+    async def async_step_user(self, user_input=None):
         if self._async_current_entries():
             return self.async_abort(
                 reason="single_instance"
             )
 
-        return self.async_create_entry(
-            title="Public IP",
-            data={},
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Public IP",
+                data=user_input,
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        CONF_PROVIDER,
+                        default="ipify",
+                    ): vol.In(PROVIDERS)
+                }
+            ),
         )
